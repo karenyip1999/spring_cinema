5d9802fa740c047fa13eca6eb22dd6ae86500dab
Karen Yip <karenyip1999@gmail.com>
2024-02-27 18:55:57 +0000

    Changes following testing

--- src/main/java/com/example/cinema/services/MovieService.java
+++ src/main/java/com/example/cinema/services/MovieService.java
@@ -17,7 +17,7 @@ public class MovieService {
 
     public String addMovie(Movie movie) {
         movieRepository.save(movie);
-        return "This movie " + movie.getTitle() + " has been added to the database.";
+        return "The movie " + movie.getTitle() + " has been added to the database.";
     }
 
 }
